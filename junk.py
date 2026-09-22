848960b0fac3aff18a8c40ea68ba5ef80cd18b2d
grkbrk <lydialyla@gmail.com>
2026-09-22 11:41:03 +0200

    Old code

--- junk.py
+++ junk.py
@@ -3,3 +3,9 @@
 #    L[i,i] = L[i,i] * (x(t[i+1]) - x(t[i-1])) / dt    #Central difference
 #L[0,0]   = L[0,0]   * (x(t[1])  - x(t[0]))  / dt      #Forward difference for first point
 #L[-1,-1] = L[-1,-1] * (x(t[-1]) - x(t[-2])) / dt      #Backward for last point
+
+
+#Box quadrate rule 
+BQR = np.eye(n) 
+BQR[0,0] = 1/2
+BQR[-1,-1] = 1/2
