3a0db416c6eaa2f0ffce2e6f4c8df172f7b33e73
grkbrk <lydialyla@gmail.com>
2026-09-22 11:40:57 +0200

    Matrixes constructed

--- Deblurring.py
+++ Deblurring.py
@@ -4,32 +4,37 @@ import math;
 a = -1                          #Upper bound
 b = 1                           #Lower bound
 n = 10                          #Number of samples
-t = np.linspace(a,b,n)          #Time array
+t = np.linspace(a,b,n)          #Time array (equidistant)
 dt= t[1]-t[0]                   #Time step
-xt = np.exp(t)*np.sin(np.pi*t)  #x at time t array
+x = np.exp(t)*np.sin(np.pi*t)   #x at time t array
 
-def x(t):                       #True signal
+#True signal
+def xt(t):                       
     return np.exp(t)*np.sin(np.pi*t)
 
-#IS THIS WHERE K SHOULD BE USED SOMEHOW?
-#Box quadrate rule 
-A = np.eye(n) #Matrix A (deblurring operator)
-A[0,0] = 1/2
-A[-1,-1] = 1/2
-
-B = 1 #In Gaussian kernel, choose sufficiently large.
-c = 1 #Const, which value to use?
+#Gaussian kernel
+B = 1   #In Gaussian kernel, choose sufficiently large. 1D
+c = 1   #Const, which value to use?
 def K(s,t):     #Gaussian kernel (point spread function (PSF))
     return c*np.exp(-np.transpose(s-t) @ B @ (s-t)) #Or should the last s be x????
 
-Ndit = np.random.normal(0, 1, n)  #Normal distribution
-nl = 0.5    #Noise level
-e = nl*Ndit #Noise
-b = A@xt + e #Observed data
+#A = K(s_i,t_i)*dt , s = t.
+A = np.zeros((n,n))
+for i in range(n):
+    for j in range(n):
+        A[i,j] = K(t[i],t[j]) * dt
+
+#Error
+Ndist = np.random.normal(0, 1, n)  #Normal distribution
+nl = 0.5            #Noise level
+e = nl * Ndist       #Noise
+
+#Observed data
+b = A @ x + e    
 
 #Reconstruct signal bu solving regularized lsq prob (1.3) using GSVD of (A,L)
 #where L is fin-diff approx of first derivative.
 L = np.eye(n-1,n)
 L = L-np.eye(n-1,n,k=1)
 
-min(np.linalg.norm(A@xt-b))
+min(np.linalg.norm(A @ x - b))
