310fc112fa770898aa175049083942dcd8e285ea
grkbrk <lydialyla@gmail.com>
2026-09-22 15:19:03 +0200

    gsvd inmplemented in deblurring

--- Deblurring.py
+++ Deblurring.py
@@ -1,5 +1,5 @@
-import numpy as np;
-import math;
+import numpy as np
+import gsvd
 
 a = -1                          #Upper bound
 b = 1                           #Lower bound
@@ -7,6 +7,8 @@ n = 10                          #Number of samples
 t = np.linspace(a,b,n)          #Time array (equidistant)
 dt= t[1]-t[0]                   #Time step
 x = np.exp(t)*np.sin(np.pi*t)   #x at time t array
+
+lb = 0.5 #0<=lambda<=1
 
 #True signal
 def xt(t):                       
@@ -30,11 +32,24 @@ nl = 0.5            #Noise level
 e = nl * Ndist       #Noise
 
 #Observed data
-b = A @ x + e    
+b = A @ x + e 
 
-#Reconstruct signal bu solving regularized lsq prob (1.3) using GSVD of (A,L)
-#where L is fin-diff approx of first derivative.
+#L is fin-diff approx of first derivative
 L = np.eye(n-1,n)
 L = L-np.eye(n-1,n,k=1)
+
+#Reconstruct signal bu solving regularized lsq prob (1.3) using 
+#GSVD of (A,L)
+U, V, C, S, W, Winv = gsvd(A,L)
+        #A = U @ C @ Winv
+        #L = V @ S @ Winv
+
+#My = Nb
+M = np.transpose(C) @ C + lb^2*(np.transpose(S) @ S)
+N = np.transpose(C) @ U
+
+y = np.invert(M) @ N @ b
+
+x = W @ y
 
 min(np.linalg.norm(A @ x - b))
